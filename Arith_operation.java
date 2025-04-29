679b3c6e9d55f849238a8ea108d60a7c2c1b0f33
Kasturi1805 <swapnagokul8@gmail.com>
2025-04-29 11:25:31 -0500

    save

---
+++ Arith_operation.java
@@ -0,0 +1,22 @@
+
+
+public class Arith_operation {
+
+    public static void main(String[] args) {
+        int a=5,b=10;
+        int sum = a+b;
+        int substraction = a-b;
+        int multiplication = a*b;
+        int division = a/b;
+        
+        System.out.println("Addition of two numbers is :" +sum);
+        System.out.println("Substraction of two numbers is :" +substraction);
+        System.out.println("Multiplication of two numbers is :" +multiplication);
+        System.out.println("Division of two numbers is :" +division);
+       
+       
+    }
+
+
+    
+}
