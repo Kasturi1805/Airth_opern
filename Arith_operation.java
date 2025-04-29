a939b48a487e5770ad62a649ddb7d47a62d32250
Kasturi1805 <swapnagokul8@gmail.com>
2025-04-29 11:29:41 -0500

    file

--- Arith_operation.java
+++ Arith_operation.java
@@ -8,11 +8,19 @@ public class Arith_operation {
         int substraction = a-b;
         int multiplication = a*b;
         int division = a/b;
+        int sqrt = a*a;
+        int cube_root = b*b*b;
+
+
         
         System.out.println("Addition of two numbers is :" +sum);
         System.out.println("Substraction of two numbers is :" +substraction);
         System.out.println("Multiplication of two numbers is :" +multiplication);
         System.out.println("Division of two numbers is :" +division);
+        System.out.println("Square Root of a number is :" +sqrt);
+        System.out.println("Cube Root of a number is :" +cube_root);
+        System.out.println("Power of a number is :" +(5^3));
+
        
        
     }
